8ad4837994f8a70028d6c2f9e72fdd59dc41ee40
Ryan Salvador <ryansalvador@gmail.com>
2018-04-18 14:37:39 +1200

    Added Mongoose

--- config/express.js
+++ config/express.js
@@ -25,6 +25,9 @@ module.exports = function () {
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
     app.use(express.static('./public'));
+
     require('../app/routes/index.server.routes.js')(app);
+    require('../app/routes/users.server.routes.js')(app);
+
     return app;
 };
