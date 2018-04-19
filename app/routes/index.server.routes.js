c23fb8e24c3b29e0c38aeff6d73d5528a88ebd21
Ryan Salvador <ryansalvador@gmail.com>
2018-04-19 15:42:00 +1200

    Add Angular Base Setup

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -3,6 +3,6 @@ const index = require('../controllers/index.server.controller');
 
 // Define the routes module' method
 module.exports = function(app) {
-	// Mount the 'index' controller's 'render' method
-	app.get('/', index.render);
+    // Mount the 'index' controller's 'render' method
+    app.get('/*', index.render);
 };
---
+++ public/systemjs.config.js
@@ -0,0 +1,45 @@
+(function (global) {
+    // packages tells the System loader how to load when no filename and/or no extension
+    var packages = {
+        app: {
+            main: './bootstrap.js',
+            defaultExtension: 'js'
+        },
+        rxjs: {
+            defaultExtension: 'js',
+            main: 'Rx.js'
+        }
+    };
+
+    // map tells the System loader where to look for things
+    var map = {
+        // angular bundles
+        '@angular': 'lib/@angular',
+        // other libraries
+        'rxjs': 'lib/rxjs'
+    };
+
+    var ngPackageNames = [
+        // paths serve as alias
+        'common',
+        'compiler',
+        'core',
+        'forms',
+        'http',
+        'router',
+        'platform-browser',
+        'platform-browser-dynamic',
+        ];
+
+    ngPackageNames.forEach(function (pkgName) {
+        packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js'};
+    });
+
+    System.config({
+        defaultJSExtension: true,
+        transpiler: null,
+        packages: packages,
+        map: map
+    });
+
+})(this);
